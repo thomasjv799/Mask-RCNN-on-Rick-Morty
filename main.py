03c9179038c5ad8b607f3971a4bad940b868624d
Thomas J Varghese <tvarghese@reactdx.com>
2023-08-03 00:58:22 +0530

    Test 2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def add(x, y)
+def add(x, y):
   return x + y
 
-def subtract(x, y):
+def subtract(x, y)
   return x - y
 
-def multiply(x, y):
+def multiply(x, y)
   return x * y
 
 def divide(x, y):
